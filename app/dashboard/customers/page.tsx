4caa2993d9587a10a5db9baf862cdb0d2ae603c8
Emily Mendez <emilypmendez@gmail.com>
2024-11-10 14:51:26 -0500

    completed ch16
    completed tutorial in full

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,13 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
 export default function Page() {
-    return <p>Customers Page</p>;
+    return (
+      <div>
+        <h1>Customers</h1>
+      </div>
+    );
   }
